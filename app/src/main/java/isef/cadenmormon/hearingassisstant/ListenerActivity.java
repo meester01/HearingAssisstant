9c9c2a8a8eead332e9a3eae23a4e435be9474271
meester01 <cadenm012@gmail.com>
2016-01-31 20:54:16 -0800

    Initial commit

---
+++ app/src/main/java/isef/cadenmormon/hearingassisstant/ListenerActivity.java
@@ -0,0 +1,22 @@
+package isef.cadenmormon.hearingassisstant;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ListenerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
